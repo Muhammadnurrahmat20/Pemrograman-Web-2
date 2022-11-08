702b132cc332373427d5dc8f5a6448c4ba09b563
Muhammadnurrahmat20 <mnurrahmat20@mhs.akba.ac.id>
2022-11-08 10:48:40 +0800

    commit keempat

---
+++ praktikum-02-02-array/array_2.php
@@ -0,0 +1,15 @@
+<?php
+$programmer_php = array ("Andi","Budi","Gunawan","Baco","Becce","Fatma");
+$programmer_python = array ("Andi","Fatma","Fadli","Haris","Baco");
+  
+  //Bagian A
+    sort($programmer_php);
+      $countProgrammerPhp = count($countProgrammerPhp);
+        echo "Bagian A : <br>";
+        echo "<ul>";
+          for($i = 0; $i < $countProgrammerPhp; $i++)
+          {
+            echo <"li">.$programmer_php[$i]."<br>";
+          }
+        echo "</u>";
+?>
